f6a5ddb80a59815ce4086b87e7c6a4823216c442
mclloyd v <mclloyd v>
2017-04-04 17:59:52 +0100

    bootstrap ui, new recipe form

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 (function () {
-    var search = document.getElementById('search')
+    var search = getId('search')
+    var submitRecipeBtn = getId('newRecipeSubmit')
+    var recipeForm = getId('recipeForm')
 
     search.addEventListener('keypress', function (e) {
         event.preventDefault();
@@ -8,6 +10,26 @@
             search.value += e.key
         }
     })
+
+    submitRecipeBtn.addEventListener('click', function(e){
+        var newRecipe = {
+            "name": getId('recipeName').value,
+            "description": getId('description').value,
+            "ingredients": [{
+                "name": getId('ingredient').value,
+                "amount": getId('amount').value,
+                "amount_type": "tons"
+            }],
+            "cusine": getId('cusine').value,
+            "portions": getId('portions').value,
+            "instructions": getId('instructions').value
+        }
+        onAddRecipe(newRecipe)
+    })
+
+    function getId(id){
+        return document.getElementById(id);
+    }
 
     function onSearch() {
         var searchTerm = { "ingredient": search.value }
@@ -21,5 +43,18 @@
             }
         }
         xhr.send(JSON.stringify(searchTerm))
+    }
+
+    function onAddRecipe(newRecipe) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:3000/new', true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                alert(response)
+            }
+        }
+        xhr.send(JSON.stringify(newRecipe))
     }
 })();
